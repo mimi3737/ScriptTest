99926451a166822cf68c8959666a5cce9e83ba15
mimi3737 <110280265+mimi3737@users.noreply.github.com>
2022-08-11 11:33:14 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53; //魔法攻撃
+
+
+    // MAGIC関数
+    public void MAGICK()
+    {
+        if(mp >=5)
+        {
+            //mpが5以上の場合
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りのMPは" + mp);
+            
+
+        }
+        else
+        {
+            //mpが足りない場合
+            
+            Debug.Log("MPが足りないため、魔法が使えない。" );
+
+        }
+    }
+
+
+}
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // MAGIC用の関数を呼び出す
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+        lastboss.MAGICK();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
